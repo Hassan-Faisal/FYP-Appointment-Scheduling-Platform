017148b4da2a1c7d08f5bb07de6299c689e85980
Hassan-Faisal <iamhassanfaisal@gmail.com>
2026-01-14 17:10:56 +0500

    main functionalities added

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,7 +5,9 @@ from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash_password(password: str):
+    password = password[:72]
     return pwd_context.hash(password)
 
 def verify_password(password: str, hashed: str):
